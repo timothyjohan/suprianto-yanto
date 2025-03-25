935123c7196332af932851632dfbf47ac411fae2
lthreen <timothyjohan11@gmail.com>
2025-03-26 00:29:38 +0700

    Booru

--- jokes/jokes.js
+++ jokes/jokes.js
@@ -1,7 +1,6 @@
 module.exports = [
-    "Kenapa komputer sering ngambek? Karena sering crash!",
-    "Kenapa ayam nyebrang jalan? Karena dia mau ke seberang, Pak!",
-    "Kenapa programmer nggak bisa pacaran? Karena dia selalu stuck di friendzone!",
-    "Kenapa bapak-bapak suka kopi? Karena hidup butuh strong!",
-    "Kenapa AC nggak pernah marah? Karena dia adem terus."
+    "Dari utara jalan ke selatan, looting besi saya juara, tak bisa apa-apa tanpa jembatan, karena saya adalah orang- . . .",
+    "Kenapa tukang bubur suka senang? karena sering halusi nasi",
+    "Ikan apa yang kalo berenang selalu nabtrak? Tuna netra",
+    "Mouse logitech apa yang dilarang di indonesia? Mouse logitech G30S PKI.",
 ];
